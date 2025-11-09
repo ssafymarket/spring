6a27786512ba9e59211f77ffca2aad2c8948d398
ari <kjk9858@gmail.com>
2025-11-09 21:01:20 +0900

    feat : 스프링 프로젝트 생성

---
+++ Ssafymarket/src/main/java/org/ssafy/ssafymarket/SsafymarketApplication.java
@@ -0,0 +1,13 @@
+package org.ssafy.ssafymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafymarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafymarketApplication.class, args);
+    }
+
+}
---
+++ Ssafymarket/src/test/java/org/ssafy/ssafymarket/SsafymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.ssafymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafymarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
